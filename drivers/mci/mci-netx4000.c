Hilscher/netx4000-barebox

commit 35873125d22bfc268efa798dd7af37c9d30e0569

mci: Use external card detect pin for detection, as in linux drivers, as SD cards might not have a pull-up on DAT3 (seems missing in specification)

drivers/mci/mci-netx4000.c
@@ -510,7 +510,7 @@ static int sdmmc_card_present(struct mci_host *mci)
 	u32 reg;
 
 	reg = readl(&regs->sd_info1);
-	if (reg & MSK_NX4000_SDIO_SD_INFO1_INFO10) /* check DAT3 level */ //reg &= 0x0020u;	/* check CD level */
+	if (reg & MSK_NX4000_SDIO_SD_INFO1_INFO5)  /* check CD level */
 		return 1; /* inserted */
 
 	return 0;

commit a890c15a1c07329f72e9238736f4580358e87c56

netx4000,mci: Card detect does not work if barebox is inside SPI flash.
This is cause by a card detect timer of the sdio module which samples CD
pin for approximately 165ms before setting card detect state.

Bug has been introduced by change 35873125d22bfc268efa798dd7af37c9d30e0569

drivers/mci/mci-netx4000.c
@@ -495,7 +495,7 @@ static int sdmmc_init(struct mci_host *mci, struct device_d *dev)
 	writel( tmpreg, &regs->sd_info1);
 	writel( 0x00000000, &regs->sd_info2);
 	writel( 0x00000000, &regs->sdio_info1);
-	writel( 0x0000C0EE, &regs->sd_option);/* max. timeout */
+	writel( 0x0000C0E0, &regs->sd_option);/* max. timeout, minimum card detect timeout (clk*2^10 = 10,2 us) */
 
 	sdmmc_set_clock(host, 400000);
 	writel( ~0x10,&regs->sd_info1);
@@ -508,6 +508,9 @@ static int sdmmc_card_present(struct mci_host *mci)
 	struct sdmmc_host *host = to_sdmmc_host(mci);
 	struct netx4000_sdio_reg __iomem *regs = host->regs;
 	u32 reg;
+
+	/* Wait for 50us to make sure card detect has been sampled at least once correctly */
+	udelay(50);
 
 	reg = readl(&regs->sd_info1);
 	if (reg & MSK_NX4000_SDIO_SD_INFO1_INFO5)  /* check CD level */
@@ -540,6 +543,8 @@ static int sdmmc_probe(struct device_d *dev)
 	mci->hw_dev = dev;
 	mci->f_max = clk_get_rate(host->clk);
 	mci->f_min = clk_get_rate(host->clk)/512;
+
+	writel( 0x0000C0E0, &host->regs->sd_option);/* max. timeout, minimum card detect timeout (clk*2^10 = 10,2 us) */
 
 	/* setup function pointer */
 	mci->init = sdmmc_init;